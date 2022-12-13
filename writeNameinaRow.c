105fbde0354f0265e413541ad7f829d4b20fd4f5
kubraakmansoy <119291045+kubraakmansoy@users.noreply.github.com>
2022-12-13 10:36:13 +0300

    Update writeNameinaRow.c

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -6,12 +6,12 @@ int main(){
  scanf("%d",&n);
  
  for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
+     for(j=0;j<n;j++){
+     printf("kubra ");
      
  }
-     printf("Akif")
+   printf("\n\n");
  }
  
-    
+    return 0;
 }
