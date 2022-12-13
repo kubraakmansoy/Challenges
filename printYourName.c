4d42ae30351e9ba6b1d244d88703cc19cb43715f
kubraakmansoy <119291045+kubraakmansoy@users.noreply.github.com>
2022-12-13 10:12:33 +0300

    Kubra Akmansoy

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,19 @@
-// your name surname and age from user
+#include<stdio.h>
+int main (){
+    char name [10],surname[10] ;
+    int age ;
+    printf("enter your name\n");
+    scanf("%s",name);
+
+    printf("enter your surname\n");
+    scanf("%s",surname);
+
+     printf("enter your age\n");
+    scanf("%d",&age);
+
+     printf("your name %s your surname %s your age %d\n",name,surname,age);
+
+     return 0;
+
+
+}
